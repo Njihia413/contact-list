60b42b2be6f7d3ec9a045bff719eb9dc88e8c2e2
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-02 12:20:29 +0300

    Add function to read from the text file and read data from it

---
+++ Lorem/readFiles.py
@@ -0,0 +1,10 @@
+text_file = "test.txt"
+def read_file(text_file):
+
+    try:
+        with open(text_file,"r") as handle:
+            data = handle.read()
+            return data
+
+    except FileNotFoundError:
+        return None        
